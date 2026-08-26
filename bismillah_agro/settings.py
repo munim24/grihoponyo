41f56c7f6e7cd6325437eea71ef4659a27f1a9e6
Munim Shahriyar <mdmunimshahriyar@gmail.com>
2026-08-26 12:01:11 +0600

    Change DATABASE_URL retrieval method in settings.py

--- bismillah_agro/settings.py
+++ bismillah_agro/settings.py
@@ -106,7 +106,7 @@ WSGI_APPLICATION = 'bismillah_agro.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=env('DATABASE_URL'),
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=False
     )
